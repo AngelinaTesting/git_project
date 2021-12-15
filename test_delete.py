0ae26f4aed06726bf48b52b499b38e587a41415c
AngeGit17 <simpleshirokova1@gmail.com>
2021-12-15 18:12:08 +0300

    My second commit

--- test_delete.py
+++ test_delete.py
@@ -1,2 +1,56 @@
-install pytest
-install requests
+import requests
+import pytest
+
+
+def test_delete_positive():
+    url = "https://petstore.swagger.io/v2/pet"
+    request = {}
+
+    request['name'] = "Selim"
+
+    request['category'] = {}
+    request['category']['name'] = "Angelina"
+    request['category']['id'] = "30121993"
+
+    request['photoUrls'] = ['www.photoPlusha', 'www.photoPlusha_1']
+    print("request", request)
+
+    response = requests.post(url, json=request)
+    print("response", response.json())
+
+    # Проверяем, что вернулся не пустой id
+    assert response.json()['id'] is not None
+    # Проверяем, что имя в ответе совпадает с имеем в запросе
+    assert response.json()['name'] == request['name']
+
+    # Проверяем создание питомца методом get
+    urlGet = "https://petstore.swagger.io/v2/pet/" + str(response.json()['id'])
+    print("urlGet = ", urlGet)
+    responseGet = requests.get(urlGet)
+    print("responseGet = ", responseGet.json())
+    # Проверяем, что id в post и get равны
+    assert responseGet.json()['id'] == response.json()['id']
+    # Проверяем, что имя в post и get одинаковые
+    assert responseGet.json()['name'] == request['name']
+
+    # Удаляем созданного питомца
+    urlDelete = "https://petstore.swagger.io/v2/pet/" + str(responseGet.json()['id'])
+    print("urlDelete = ", urlDelete)
+    responseDelete = requests.delete(urlDelete)
+    print("responseDelete = ", responseDelete.json())
+    assert responseDelete.json()['code'] == 200
+
+    # Проверяем, что питомец удалился
+    urlGet_2 = "https://petstore.swagger.io/v2/pet/" + str(responseDelete.json()['message'])
+    print("urlGet_2 = ", urlGet_2)
+    responseGet_delete = requests.get(urlGet_2)
+    print("responseGet_delete = ", responseGet_delete.json())
+
+    # Удаление удаленного питомца
+# urlDelete_2 = "https://petstore.swagger.io/v2/pet/" + str(responseGet.json()['id']) # в str передается id из post,провереный get
+# print("urlDelete_2 = ", urlDelete_2)
+# responseDelete_2 = requests.delete(urlDelete_2)
+# print("responseDelete_2 = ", responseDelete_2.json())
+
+# assert responseDelete_2.json()['code'] == 200
+
