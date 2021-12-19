4f0a42868b509d3be5f227a70d5824270e002a93
AngeGit17 <simpleshirokova1@gmail.com>
2021-12-19 15:07:43 +0300

    My 12 commit

---
+++ test_delete_negative_user.py
@@ -0,0 +1,10 @@
+import requests
+import pytest
+
+# Удаление несуществующего пользователя
+def test_delete_negative_user():
+    urlDelete = "https://petstore.swagger.io/v2/user/Ange_tester_7"
+    print("urlDelete = ", urlDelete)
+    responseDelete = requests.delete(urlDelete)
+    print("responseDelete = ", responseDelete.json())
+    # assert responseDelete.json()['message'] == 'Error: response status is 404'
