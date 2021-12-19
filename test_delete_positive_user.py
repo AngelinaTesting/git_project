96ea720d7c1d5becdfa25e64f92b191b9838a7fe
AngeGit17 <simpleshirokova1@gmail.com>
2021-12-19 14:41:07 +0300

    My 11 commit

--- test_delete_positive_user.py
+++ test_delete_positive_user.py
@@ -2,47 +2,54 @@ import requests
 import pytest
 
 
-def test_delete_positive():
-    url = "https://petstore.swagger.io/v2/pet"
+def test_delete_positive_user():
+    url = "https://petstore.swagger.io/v2/user"
     request = {}
+    request['id'] = "3012199371"
+    request['username']= "Ange_tester_7"
+    request['firstname'] = "Angelinochka"
+    request['lastname']= "Angela"
+    request['email'] = "1@mail.ru"
+    request['password'] = "134567"
+    request['password'] = "11111111111"
+    request['userStatus'] = '0'
 
-    request['name'] = "Lola"
-
-    request['category'] = {}
-    request['category']['name'] = "Angelina"
-    request['category']['id'] = "30121993"
-
-    request['photoUrls'] = ['www.photoPlusha', 'www.photoPlusha_1']
     print("request", request)
 
     response = requests.post(url, json=request)
-    print("response", response.json())
+    print("response =", response.json())
+    assert response.json()['code'] == 200
+    assert response.json()['message'] == request['id']
 
-    # Проверяем, что вернулся не пустой id
-    assert response.json()['id'] is not None
-    # Проверяем, что имя в ответе совпадает с имеем в запросе
-    assert response.json()['name'] == request['name']
-
-    # Проверяем создание питомца методом get
-    urlGet = "https://petstore.swagger.io/v2/pet/" + str(response.json()['id'])
+    # Проверяем методом get, что созданный пользователь может залогиниться
+    urlGet = "https://petstore.swagger.io/v2/user/login?username=Ange_tester_7&password=134567"
     print("urlGet = ", urlGet)
     responseGet = requests.get(urlGet)
     print("responseGet = ", responseGet.json())
-    # Проверяем, что id в post и get равны
-    assert responseGet.json()['id'] == response.json()['id']
-    # Проверяем, что имя в post и get одинаковые
-    assert responseGet.json()['name'] == request['name']
 
-    # Удаляем созданного питомца
-    urlDelete = "https://petstore.swagger.io/v2/pet/" + str(responseGet.json()['id'])
+    assert responseGet.json()['code'] == 200
+    assert responseGet.json()['message'] is not None
+
+    # Удаляем созданного пользователя
+    urlDelete = "https://petstore.swagger.io/v2/user/" + str(request['username'])
     print("urlDelete = ", urlDelete)
     responseDelete = requests.delete(urlDelete)
     print("responseDelete = ", responseDelete.json())
-    assert responseDelete.json()['code'] == 200
 
-    # Проверяем, что питомец удалился
-    urlGet_2 = "https://petstore.swagger.io/v2/pet/" + str(responseDelete.json()['message'])
-    print("urlGet_2 = ", urlGet_2)
-    responseGet_delete = requests.get(urlGet_2)
-    print("responseGet_delete = ", responseGet_delete.json())
-    assert responseGet_delete.json()['message'] == 'Pet not found'
+    assert responseDelete.json()['code'] == 200
+    assert responseDelete.json()['message'] == request['username']
+
+    # Проверяем, что удаленный пользователь не может залогиниться.
+    # Эта проверка будет FAIL, потому что методом Get/user/login (из моего варианта) при любых условиях пользователь может залогиниться
+    #urlGet_2 = "https://petstore.swagger.io/v2/user/login?username=Ange_tester_7&password=134567"
+    #print("urlGet_2 = ", urlGet_2)
+    #responseGet_delete = requests.get(urlGet_2)
+    #print("responseGet_delete = ", responseGet_delete.json())
+    #assert responseGet_delete.json()['code'] == 400
+
+    #Проверяем методом Get/user/username (не из моего варианта курсовой), что пользователь действительно удален
+    urlGet_3 = "https://petstore.swagger.io/v2/user/"+ str(responseDelete.json()['message']) # передаем имя пользователя из ответа в delete
+    print("urlGet_3 = ", urlGet_3)
+    responseGet_delete_2 = requests.get(urlGet_3)
+    print("responseGet_delete_2 = ", responseGet_delete_2.json())
+    assert responseGet_delete_2.json()['message'] == 'User not found'
