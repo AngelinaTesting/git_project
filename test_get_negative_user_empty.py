09e7c6e22ddae22b6874344b12c213d6a92ec1ce
AngeGit17 <simpleshirokova1@gmail.com>
2021-12-19 17:49:14 +0300

    My 1 commit

---
+++ test_get_negative_user_empty.py
@@ -0,0 +1,30 @@
+import requests
+import pytest
+
+
+def test_get_negative_user_empty():
+    url = "https://petstore.swagger.io/v2/user"
+    request = {}
+    request['id'] = "3012199371"
+    request['username']= "Ange_tester"
+    request['firstname'] = "Angelinochka"
+    request['lastname']= "Angela"
+    request['email'] = "1@mail.ru"
+    request['password'] = "134567"
+    request['password'] = "11111111111"
+    request['userStatus'] = '0'
+
+    print("request", request)
+
+    response = requests.post(url, json=request)
+    print("response =", response.json())
+    assert response.json()['code'] == 200
+    assert response.json()['message'] == request['id']
+
+
+    urlGet = "https://petstore.swagger.io/v2/user/login?username=&password="
+    print("urlGet = ", urlGet)
+    responseGet = requests.get(urlGet)
+    print("responseGet = ", responseGet.json())
+
+    assert responseGet.json()['code'] == 404
