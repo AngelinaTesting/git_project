e4d11eed167c60a58eb54d2a0057b5f1f34b69e5
AngeGit17 <simpleshirokova1@gmail.com>
2021-12-19 17:13:39 +0300

    My 1 commit

---
+++ test_post_negative_id_non_integer.py
@@ -0,0 +1,21 @@
+import requests
+import pytest
+
+
+def test_delete_positive_user():
+    url = "https://petstore.swagger.io/v2/user"
+    request = {}
+    request['id'] = "ывпва"
+    request['username']= "Ange_tester_4"
+    request['firstname'] = "Angelinochka"
+    request['lastname']= "Angela"
+    request['email'] = "1@mail.ru"
+    request['password'] = "134567"
+    request['password'] = "11111111111"
+    request['userStatus'] = '0'
+
+    print("request", request)
+
+    response = requests.post(url, json=request)
+    print("response =", response.json())
+    assert response.json()['code'] == 400 # в Postman ошибка 400, тут 500
