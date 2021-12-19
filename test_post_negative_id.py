d39013339ea1c0ae6ba37d9fb5313aaecc5b399f
AngeGit17 <simpleshirokova1@gmail.com>
2021-12-19 17:20:39 +0300

    My 1 commit

--- test_post_negative_id.py
+++ test_post_negative_id.py
@@ -5,11 +5,11 @@ import pytest
 def test_post_negative_id_():
     url = "https://petstore.swagger.io/v2/user"
     request = {}
-    request['id'] = "ывпва"
+    request['id'] = "12394948449.4"
     request['username']= "Ange_tester_4"
     request['firstname'] = "Angelinochka"
     request['lastname']= "Angela"
-    request['email'] = "1@mail.ru"
+    request['email'] = "2@mail.ru"
     request['password'] = "134567"
     request['password'] = "11111111111"
     request['userStatus'] = '0'
@@ -18,4 +18,4 @@ def test_post_negative_id_():
 
     response = requests.post(url, json=request)
     print("response =", response.json())
-    assert response.json()['code'] == 400 # в Postman ошибка 400, тут 500
+    assert response.json()['id'] == request['id'] # в Postman код 200 и обрезаются цифры после .
