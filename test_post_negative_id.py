b742b01bf4b9c5dc02254b0d9ded2f04b7038b84
AngeGit17 <simpleshirokova1@gmail.com>
2021-12-20 14:54:39 +0300

    My last commit

--- test_post_negative_id.py
+++ test_post_negative_id.py
@@ -10,12 +10,15 @@ def test_post_negative_id_():
     request['firstname'] = "Angelinochka"
     request['lastname']= "Angela"
     request['email'] = "2@mail.ru"
-    request['password'] = "134567"
-    request['password'] = "11111111111"
+    request['password'] = "1237"
+    request['phone'] = "11111111111"
     request['userStatus'] = '0'
 
     print("request", request)
 
     response = requests.post(url, json=request)
     print("response =", response.json())
-    assert response.json()['id'] == request['id'] # в Postman код 200 и обрезаются цифры после .
+    # assert response.json()['id'] == request['id'] в Postman приходит успешный код 200 и обрезаются цифры после .
+
+    assert response.json()['code'] == 500  # в Postman ошибка 400, тут 500
+    assert response.json()['message'] == 'something bad happened'
