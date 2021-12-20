cd1147f6dcbee0959b0b14770d98d086702f4990
AngeGit17 <simpleshirokova1@gmail.com>
2021-12-20 14:56:49 +0300

    My last commit

--- test_post_negative_userstasus_not_int.py
+++ test_post_negative_userstasus_not_int.py
@@ -1,7 +1,6 @@
 import requests
 import pytest
 
-# Проверяем, что обрезаются цифры после точки
 def test_post_negative_userStatus_not_int():
     url = "https://petstore.swagger.io/v2/user"
     request = {}
@@ -10,12 +9,14 @@ def test_post_negative_userStatus_not_int():
     request['firstname'] = "Angelinochka"
     request['lastname']= "Angela"
     request['email'] = "2@mail.ru"
-    request['password'] = "134567"
-    request['password'] = "11111111111"
+    request['password'] = "1237"
+    request['phone'] = "11111111111"
     request['userStatus'] = 'fdgd'
 
     print("request", request)
 
     response = requests.post(url, json=request)
     print("response =", response.json())
-    assert response.json()['code'] == 400  # в Postman 400 ошибка
+    # assert response.json()['code'] == 400   в Postman 400 ошибка
+    assert response.json()['code'] == 500
+    assert response.json()['message'] == 'something bad happened'
